d2d2b3597d1e33ea7031f5f63c9ae3a8bcef79e1
Pedro Zamecki <51971866+PedroZamecki@users.noreply.github.com>
2024-07-22 12:13:09 -0300

    First Commit

---
+++ src/main/java/org/zamecki/nm/NaturalMobs.java
@@ -0,0 +1,19 @@
+package org.zamecki.nm;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NaturalMobs implements ModInitializer {
+    public static final String MOD_ID = "nm";
+    public static final Logger LOGGER = LoggerFactory.getLogger("NaturalMobs");
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("Hello Fabric world!");
+    }
+}
